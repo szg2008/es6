5c6618db1c2118cd65daab7785caa185269cc21c
szg2008 <l>
2016-12-08 15:33:15 +0800

    chapter4

--- chapter4/js/es5.js
+++ chapter4/js/es5.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var _templateObject = _taggedTemplateLiteral(['Hello ', ' world ', ''], ['Hello ', ' world ', '']),
+    _templateObject2 = _taggedTemplateLiteral(['The total is ', ' (', ' with tax)'], ['The total is ', ' (', ' with tax)']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
 /**
  * 第4章  字符串的扩展
  */
@@ -104,5 +109,75 @@ for (var i = 0; i < text.length; i++) {
 }
 
 //at和ES5中的charat方法类似
-console.log('str'.charAt(0));
-console.log('str'.at(0));
+console.log('str'.charAt(0)); //s
+console.log('str'.at(0)); //s
+
+/**
+ *
+ * includes startsWith endsWith,都支持2个参数
+ * 其中，endsWith表示的是前n个字符
+ * includes和startsWith表示从第n个位置直到字符串结束的字符
+ * **/
+var s = 'Hello World';
+console.log(s.includes('Hello')); //true
+console.log(s.startsWith('He')); //true
+console.log(s.endsWith('ld1')); //false
+console.log(s.includes("World", 6)); //true 表示从第6个字符开始的字符是包含World的
+console.log(s.startsWith('llo', 2)); //true 表示从第二个字符开始是以llo开头的
+console.log(s.endsWith('ll', 4)); //true 表示前4个字符中是以ll结尾的
+
+
+//repeat返回一个新的字符串，代表将原来的字符串重复n次
+console.log('repeat'.repeat(3)); //repeatrepeatrepeat
+console.log('repeat'.repeat('2')); //repeatrepeat
+console.log('repeat'.repeat(2.6)); //先取整，然后再输出repeatrepeat
+console.log('repeat'.repeat(NaN)); //''
+console.log('repeat'.repeat(-0.2)); //''取整为0
+//console.log('repeat'.repeat(-9.3));//error
+
+
+//padStart padEnd  自动补全
+console.log('x'.padStart(9, 'abc')); //abcabcabx
+console.log('x'.padEnd(11, 'abc')); //xabcabcabca
+console.log('xxx'.padStart(2, 'a')); //xxx
+console.log('xxx'.padEnd(2, 'a')); //xxx
+console.log('xxx'.padStart(5)); //  xxx
+console.log('xxx'.padEnd(5)); //xxx
+
+//模板字符串，使用`反引号标识
+var name = "John";var addr = '北京';
+console.log(name + ' comes from ' + addr);
+console.log(1 + ' + ' + 2); //1+2
+var str = 'return ' + '`Hello ${name}`';
+var func = new Function('name', str);
+console.log(func('Jack'));
+
+//标签模板,函数被用来定义并且调用处理模板字符串
+var a = 30;var b = 15;
+tag(_templateObject, a + b, a * b);
+function tag(stringArr, value1, value2) {
+    console.log(stringArr); //模板字符串中未被变量替换的部分['Hello','World','']
+    console.log(stringArr.length);
+    console.log(value1); //a+b 45
+    console.log(value2); //a*b 450
+}
+
+var total = 30;
+var msg = pass(_templateObject2, total, total * 1.20);
+function pass(literals) {
+    var result = '';
+    var i = 0;
+    console.log(literals.raw);
+    //console.log(arguments);
+    //console.log(literals);
+    while (i < literals.length) {
+        result += literals[i++];
+        //console.log(result);
+        if (i < arguments.length) {
+            result += arguments[i];
+        }
+    }
+    return result;
+}
+
+console.log(msg);
